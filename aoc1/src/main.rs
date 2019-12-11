d1b13eea6ea1e79a5223dac1b9c6746580bdb0cd
Roberto Amorim <roberto.amorim@gmail.com>
2019-12-11 08:36:39 -0300

    AoC day 1 done

--- aoc1/src/main.rs
+++ aoc1/src/main.rs
@@ -2,23 +2,32 @@
 //     println!("Hello, world!");
 // }
 
-use std::io::{BufRead, BufReader};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn calc_fuel(mass: u64) -> u64 {
     (mass / 3).saturating_sub(2)
 }
 
+fn total_fuel(mass: u64) -> u64 {
+    let mut total = 0;
+    let mut f = calc_fuel(mass);
+    while f > 0 {
+        total += f;
+        f = calc_fuel(f);
+    }
+    total
+}
+
 fn main() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
     let buf_reader = BufReader::new(file);
-    let lines = buf_reader.lines();
-    let fuel: u64 = lines
-        .filter_map({|x|
-            x.ok()?.parse().ok()
-        })
-        .map({|mass| calc_fuel(mass)})
-        .sum();
-    println!("{}", fuel);
+    let masses: Vec<u64> = buf_reader
+        .lines()
+        .filter_map({ |x| x.ok()?.parse().ok() })
+        .collect();
+    let fuel: u64 = masses.iter().map({ |mass| calc_fuel(*mass) }).sum();
+    let total_fuel: u64 = masses.iter().map({ |mass| total_fuel(*mass) }).sum();
+    println!("{} - {}", fuel, total_fuel);
     Ok(())
 }
