c788889c517bae731002b10c00c2fd98ebbcecca
Roberto Amorim <roberto.amorim@gmail.com>
2019-12-15 11:23:24 -0300

    Day 3 part 1 done

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -1,17 +1,74 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::fmt;
 
-fn load_input() {
+fn load_input() -> Vec<Wire> {
     let file = File::open("input.txt").unwrap();
     let buf_reader = BufReader::new(file);
-    for line in buf_reader.lines() {
-        if let Ok(l) = line {
-            let v:Vec<&str> = l.split(',').collect();
-            println!("{:?}", v);
-        }
+    buf_reader.lines()
+        .filter_map(
+            |s|
+            if let Ok(line) = s {
+                Some(line_to_wire(&line))
+             } else {
+                None
+             }
+            )
+        .collect()
+}
+
+fn str_to_wiresection(x:&mut i64, y:&mut i64, s:&str) -> Option<WireSection> {
+    let (direction, sdistance) = s.split_at(1);
+    let distance:i64 = sdistance.parse().ok()?;
+    match direction {
+        "U" =>
+            {
+                let old_y = *y;
+                *y += distance;
+                Some(WireSection::Vertical(*x, old_y, *y, true))
+            },
+        "D" =>
+            {
+                let old_y = *y;
+                *y -= distance;
+                Some(WireSection::Vertical(*x, *y, old_y, false))
+            },
+        "R" =>
+            {
+                let old_x = *x;
+                *x += distance;
+                Some(WireSection::Horizontal(old_x, *y, *x, true))
+            },
+        "L" =>
+            {
+                let old_x = *x;
+                *x -= distance;
+                Some(WireSection::Horizontal(*x, *y, old_x, false))
+            },
+            _ =>
+            None
     }
 }
 
+fn str_to_wiresection_closure(x:i64, y:i64) -> impl FnMut(&str) -> Option<WireSection> {
+    let mut x = x;
+    let mut y = y;
+
+    {move |s|
+        str_to_wiresection(&mut x, &mut y, s)
+    }
+}
+
+fn line_to_wire(line:&str) -> Wire {
+    Wire(
+        line
+            .split(',')
+            .filter_map(str_to_wiresection_closure(0, 0))
+            .collect()
+    )
+}
+
+#[derive(Debug)]
 enum WireSection {
     Vertical(i64, i64, i64, bool),
     Horizontal(i64, i64, i64, bool)
@@ -25,9 +82,9 @@ impl WireSection {
             'D' =>
                 Some(WireSection::Vertical(x, y-distance, y, false)),
             'R' =>
-                Some(WireSection::Vertical(x, y, x+distance, true)),
+                Some(WireSection::Horizontal(x, y, x+distance, true)),
             'L' =>
-                Some(WireSection::Vertical(x-distance, y, x, false)),
+                Some(WireSection::Horizontal(x-distance, y, x, false)),
             _ =>
                 None
         }
@@ -48,7 +105,93 @@ impl WireSection {
     }
 }
 
+impl fmt::Display for WireSection {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            WireSection::Vertical(_, ymin, ymax, d) =>
+                {
+                    write!(f, "{}{}", if *d { "U" } else { "D" }, ymax-ymin)
+                }
+            WireSection::Horizontal(xmin, _, xmax, d) =>
+                {
+                    write!(f, "{}{}", if *d { "R" } else { "L" }, xmax-xmin)
+                }
+        }
+    }
+}
+
+struct Wire(Vec<WireSection>);
+
+impl Wire {
+    fn intersections(&self, wire: &Wire) -> Vec<(i64,i64)> {
+        let mut intersections:Vec<(i64,i64)> = Vec::new();
+        for ws in self.0.iter() {
+            for ws2 in wire.0.iter() {
+                if let Some(i) = ws.intersection(ws2) {
+                    intersections.push(i)
+                }
+            }
+        }
+        intersections
+    }
+
+    fn min_intersection_distance(&self, wire: &Wire) -> Option<i64> {
+        self.intersections(wire).iter()
+            .map(|(x, y)| x.abs() + y.abs())
+            .min()
+    }
+}
+
+impl fmt::Display for Wire {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut first = true;
+        for w in self.0.iter() {
+            if !first {
+                write!(f, ",")?;
+            } else {
+                first = false;
+            }
+            write!(f, "{}", w)?;
+        }
+        Ok(())
+    }
+}
+
 fn main() {
-    load_input();
-    println!("Day 3 Stub");
+    let wires = load_input();
+    let distance = wires[0].min_intersection_distance(&wires[1]);
+    match distance {
+        Some(d) => println!("{}", d),
+        None => println!("No intersections")
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_line_to_wire_1() {
+        let line = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+        let wire = line_to_wire(line);
+        assert_eq!(line,wire.to_string());
+    }
+
+    #[test]
+    fn test_intersection_1() {
+        let wire1 = line_to_wire("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+        let wire2 = line_to_wire("U62,R66,U55,R34,D71,R55,D58,R83");
+        let minimum_distance = wire1.min_intersection_distance(&wire2);
+        assert_eq!(minimum_distance,Some(159));
+    }
+
+    #[test]
+    fn test_intersection_2() {
+        let wire1 = line_to_wire("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+        let wire2 = line_to_wire("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+        let minimum_distance = wire1.min_intersection_distance(&wire2);
+        assert_eq!(minimum_distance,Some(135));
+    }
 }
